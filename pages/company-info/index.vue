c514c663992982e8bc8d36164bc5df46061526cd
dengxinliang <645068340@qq.com>
2019-07-18 18:05:28 +0800

    190718

--- pages/company-info/index.vue
+++ pages/company-info/index.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="nav-bg"></div>
     <div class="company_info">
-      <a-form :form="form">
+      <a-form :form="form" @submit="handleSubmit">
         <div class="company_info_form">
           <div class="title">公司基本信息</div>
           <div class="form_centen">
@@ -15,7 +15,15 @@
               :colon="false"
               :required="true"
             >
-              <a-input placeholder="请输入公司全称" size="large"></a-input>
+              <a-input 
+                placeholder="请输入公司全称" 
+                size="large"
+                :maxLength="10"
+                v-decorator="[
+                  'name',
+                  {rules: [{ required: true, message: '请输入公司名称' }]}
+                ]"
+              ></a-input>
             </a-form-item>
             <!-- 公司简称 -->
             <a-form-item
@@ -25,7 +33,14 @@
               :wrapper-col="formItemLayout4.wrapperCol"
               :colon="false"
             >
-              <a-input size="large"></a-input>
+              <a-input 
+                size="large"
+                :maxLength="10"
+                v-decorator="[
+                  'abbreviation',
+                  {rules: [{ required: false, message: '' }]}
+                ]"
+              ></a-input>
             </a-form-item>
             <!-- 公司类型 -->
             <a-form-item
@@ -36,7 +51,16 @@
               :colon="false"
               :required="true"
             >
-              <a-select defaultValue="上市公司" size="large">
+              <a-select 
+                size="large"
+                v-decorator="[
+                  'type',
+                  {
+                    initialValue: '1',
+                    rules: [{ required: true, message: '请选择公司类型' }]
+                  }
+                ]"
+              >
                 <a-select-option value="1">上市公司</a-select-option>
               </a-select>
             </a-form-item>
@@ -49,7 +73,16 @@
               :colon="false"
               :required="true"
             >
-              <a-select defaultValue="50-150人" size="large">
+              <a-select 
+                size="large"
+                v-decorator="[
+                  'scale',
+                  {
+                    initialValue: '1',
+                    rules: [{ required: true, message: '请选择公司规模' }]
+                  }
+                ]"
+              >
                 <a-select-option value="1">50-150人</a-select-option>
               </a-select>
             </a-form-item>
@@ -63,13 +96,29 @@
               :required="true"
             >
               <div class="flex">
-                <a-select defaultValue="上海" size="large">
+                <a-select 
+                  size="large"
+                  v-decorator="[
+                    'province',
+                    {
+                      initialValue: '1',
+                      rules: [{ required: true, message: '请选择省份' }]
+                    }
+                  ]"
+                >
                   <a-select-option value="1">上海</a-select-option>
                 </a-select>
-                <a-select defaultValue="上海" size="large" class="mar">
+                <a-select 
+                  size="large" 
+                  class="mar"
+                  defaultValue="1"
+                >
                   <a-select-option value="1">上海</a-select-option>
                 </a-select>
-                <a-select defaultValue="虹口区" size="large">
+                <a-select
+                  size="large"
+                  defaultValue="1"
+                >
                   <a-select-option value="1">虹口区</a-select-option>
                 </a-select>
               </div>
@@ -82,9 +131,129 @@
               :wrapper-col="formItemLayout4.wrapperCol"
               :colon="false"
             >
-              <a-input size="large"></a-input>
+              <a-input 
+                size="large"
+                :maxLength="30"
+                v-decorator="[
+                  'webpackAddress',
+                  {rules: [{ required: false, message: '' }]}
+                ]"
+              ></a-input>
+            </a-form-item>
+            <!-- 公司地址 -->
+            <a-form-item
+              class="form_item_6" 
+              label="公司地址" 
+              :label-col="formItemLayout6.labelCol"
+              :wrapper-col="formItemLayout6.wrapperCol"
+              :colon="false"
+              :required="true"
+            >
+              <a-input 
+                size="large"
+                :maxLength="30" 
+                v-decorator="[
+                  'address',
+                  {
+                    initialValue: '上海市青浦区绝俘路258弄60号（朗诵金融大厦）5层',
+                    rules: [{ required: true, message: '请输入公司地址' }]
+                  }
+                ]"
+              ></a-input>
+            </a-form-item>
+            <!-- 公司邮箱 -->
+            <a-form-item
+              class="form_item_4" 
+              label="公司邮箱" 
+              :label-col="formItemLayout4.labelCol"
+              :wrapper-col="formItemLayout4.wrapperCol"
+              :colon="false"
+            >
+              <a-input 
+                size="large"
+                :maxLength="20"
+                v-decorator="[
+                  'mailbox',
+                  {rules: [{ required: false, message: '' }]}
+                ]"
+              ></a-input>
             </a-form-item>
           </div>
+          <div class="title">公司其他信息</div>
+          <div class="form_flex">
+            <!-- 公司简介 -->
+            <a-form-item
+              label="公司简介" 
+              :label-col="{span: 2}"
+              :wrapper-col="{span: 21}"
+              :colon="false"
+              :required="true"
+            >
+              <a-textarea 
+                size="large" 
+                :autosize="{ minRows: 6, maxRows: 10 }"
+                :maxLength="100"
+                v-decorator="[
+                  'synopsis',
+                  {rules: [{ required: true, message: '请输入公司简介' }]}
+                ]"
+              ></a-textarea>
+              <p>0/100文字</p>
+            </a-form-item>
+            <!-- 公司LOGO -->
+            <a-form-item 
+              label="公司LOGO" 
+              :label-col="{span: 2}"
+              :colon="false"
+            >
+              <div class="item_flex item_logo">
+                <a-upload name="file" 
+                  :multiple="false" 
+                  listType="picture-card"
+                  :fileList="logoFileList"
+                  action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                  v-decorator="['logeImg', {
+                    valuePropName: 'logoFileList',
+                    getValueFromEvent: normFile
+                  }]"
+                  @change="logoChange"
+                >
+                  <a-button type="primary">选择上传图片</a-button>
+                </a-upload>
+              </div>
+            </a-form-item>
+            <!-- 公司图片 -->
+            <a-form-item 
+              label="公司图片" 
+              :label-col="{span: 2}"
+              :colon="false"
+            >
+              <div class="item_flex item_company">
+                <a-upload name="files" 
+                  :multiple="true" 
+                  listType="picture-card"
+                  :fileList="companyFileList"
+                  action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                  v-decorator="['companyImg', {
+                    valuePropName: 'companyFileList',
+                    getValueFromEvent: normFile
+                  }]"
+                  @change="companyChange"
+                >
+                  <a-button type="primary">选择上传图片</a-button>
+                </a-upload>
+              </div>
+            </a-form-item>
+          </div>
+        </div>
+        <!-- 提交按钮 -->
+        <div class="submit">
+            <a-button
+              type="primary"
+              html-type="submit"
+            >
+              保存
+            </a-button>
         </div>
       </a-form>
     </div>
@@ -104,19 +273,61 @@ export default {
   data() {
       return{
         form: this.$form.createForm(this),
-        formItemLayout6: {
+        formItemLayout6: { //栅格60%
           labelCol: {span: 4},
           wrapperCol: {span: 16}
         },
-        formItemLayout4: {
+        formItemLayout4: { //栅格40%
           labelCol: {span: 6},
           wrapperCol: {span: 16}
-        }
+        },
+        logoFileList: [], //logo图片列表
+        companyFileList: [] //公司图片列表
       }
   },
   mounted() {
   },
   methods: {
+    //提交时上传图片处理
+    normFile(e) {
+      if (Array.isArray(e)) {
+        return e;
+      }
+      return e && e.fileList;
+    },
+    //公司logo图片上传
+    logoChange({ file, fileList }) {
+      let newList = [...fileList];
+      newList = newList.slice(-1);
+      newList = newList.map((v) => {
+        if (v.response) {
+          v.url = v.response.url;
+        }
+        return v;
+      });
+      this.logoFileList = newList;
+    },
+    //公司图片上传
+    companyChange({ file, fileList }) {
+      let newList = [...fileList];
+      newList = newList.slice(-6);
+      newList = newList.map((v) => {
+        if (v.response) {
+          v.url = v.response.url;
+        }
+        return v;
+      });
+      this.companyFileList = newList;
+    },
+    //提交表单
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log(values);
+        }
+      });
+    }
   }
 };
 </script>

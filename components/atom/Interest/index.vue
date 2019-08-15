c64eab5284460d0beb19540ab86e6de63b7d97bb
dengxinliang <645068340@qq.com>
2019-08-16 02:01:05 +0800

    190816

--- components/atom/Interest/index.vue
+++ components/atom/Interest/index.vue
@@ -1,26 +1,21 @@
 <template>
   <section class="interest">
       <div class="title">猜您感兴趣</div>
-      <dl>
-          <dt><img src="../../../assets/images/positions_img.png" alt=""></dt>
+      <dl v-for="item in rows" :key="item.id" @click="goJobDetail(item)">
+          <dt><img :src="item.logo" alt=""></dt>
           <dd>
-              <p>赛事运营策划</p>
-              <span>20k-30k/月</span>
+              <p>{{item.careersName}}</p>
+              <span>{{item.jobPrice}}/月</span>
           </dd>
       </dl>
-      <dl>
-          <dt><img src="../../../assets/images/positions_img.png" alt=""></dt>
-          <dd>
-              <p>赛事运营策划</p>
-              <span>20k-30k/月</span>
-          </dd>
-      </dl>
-      <div class="btn"><a>查看更多推荐职位</a></div>
+      <div class="btn"><a @click="goJob">查看更多推荐职位</a></div>
   </section>
 </template>
 
 <script>
 import "./index.less";
+import ajax from '../../../plugins/api';
+import util from '../../../plugins/utils/util';
 
 export default {
   name: "Interest",
@@ -31,11 +26,37 @@ export default {
      }
   },
   data() {
-      return{}
+      return{
+          rows: []
+      }
   },
   mounted() {
+      this.devGet();
   },
   methods: {
+      devGet() {
+          let userInfo = util.getStore('userInfo');
+          ajax.get("jobs/interest", {
+              user_id: userInfo.id
+          }).then(res => {
+              if(res.retcode == 0) {
+                  this.rows = res.data || [];
+              }
+          })
+      },
+      goJob() {
+          this.$router.push({
+              path: 'search-positions'
+          })
+      },
+      goJobDetail(e) {
+          this.$router.push({
+              path: 'job-detail',
+              query: {
+                  id: e.id
+              }
+          })
+      }
   }
 };
 </script>

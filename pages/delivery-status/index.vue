f7aab8bf4b9f63f47ce6475e1061ff36beeff8ee
dengxinliang <645068340@qq.com>
2019-09-05 16:43:38 +0800

    需求修改

--- pages/delivery-status/index.vue
+++ pages/delivery-status/index.vue
@@ -16,7 +16,7 @@
                   <a v-for="(item, index) in navs" :key="index" :class="{active: active == index}" @click="handleNav(index)">
                       {{item}}
                       <ul v-if="isTrue && index == 3">
-                          <li v-for="i in interviews" :key="i.index" @click="handleInterviews(i.index)">{{i.name}}</li>
+                          <li v-for="i in interviews" :key="i.index" @click.stop="handleInterviews(i.index)">{{i.name}}</li>
                       </ul>
                   </a>
               </div>
@@ -31,10 +31,16 @@
                                       <p><span>使用简历：</span>在线简历</p>
                                   </a-col>
                                   <a-col class="right">
-                                      <p>{{item.send_time ? format(item.send_time) : item.send_time}}</p>
-                                      <div>
-                                          {{item.activeName}}
-                                          <a-icon type="caret-down" />
+                                      <div class="right_btn" v-if="item.status == 0">
+                                          <a-button type="primary" @click.stop="modify(item, 1)">接受面试</a-button>
+                                          <a-button @click.stop="modify(item, 2)">拒绝面试</a-button>
+                                      </div>
+                                      <div class="right_text">
+                                          <p>{{item.send_time ? format(item.send_time) : item.send_time}}</p>
+                                          <div>
+                                              {{item.activeName}}
+                                              <a-icon type="caret-down" />
+                                          </div>
                                       </div>
                                   </a-col>
                               </a-row>
@@ -47,25 +53,55 @@
                           </dl>
                       </div>
                       <a-timeline>
-                          <a-timeline-item color="#515ffd" v-if="item.is_interview == 1 && active == 2">
-                              <p>2019-08-35  15:20</p>
+                          <a-timeline-item v-if="item.is_read == 2 && active == 4">
+                              <p>{{format(item.unsuited_time)}}</p>
                               <dl>
                                   <dd>
-                                      <h4>简历被标记为{{item.is_read == 2 ? '不合适' : '邀面试'}}</h4>
-                                      <h6 v-if="item.is_read == 1">非常荣幸收到您的简历，经过我们评估，您与该位职位符合度很高。我们会在稍后与您电话联系， 邀请您来我司进行面试，请您保持电话畅通。 </h6>
-                                      <h6 v-else>非常荣幸收到您的简历，经过我们评估，您与该位职位不符合。感谢您投递我们公司。 </h6>
+                                      <h4>简历被标记为不合适</h4>
+                                      <h6>非常荣幸收到您的简历，经过我们评估，您与该位职位不符合。感谢您投递我们公司。</h6>
                                       <h6>----{{item.companyName}}</h6>
                                   </dd>
                               </dl>
                           </a-timeline-item>
-                          <a-timeline-item color="#969696">
-                              <p>2019-08-35  15:20</p>
+                          <a-timeline-item v-if="item.isShow && active == 3">
+                              <p>{{format(item.unsuited_time)}}</p>
+                              <dl>
+                                  <dd>
+                                      <h4>简历被标记为邀面试</h4>
+                                      <h6>非常荣幸收到您的简历，经过我们评估，您与该位职位符合度很高。我们会在稍后与用人部门继续
+协商，尽快确认是否给您发送面试邀请，请您关注后续状态。</h6>
+                                      <h6>----{{item.companyName}}</h6>
+                                  </dd>
+                              </dl>
+                          </a-timeline-item>
+                          <a-timeline-item v-if="item.isShow && active > 1">
+                              <p>{{format(item.intention_time)}}</p>
+                              <dl>
+                                  <dd>
+                                      <h4>简历被标记为有意向</h4>
+                                      <h6>非常荣幸收到您的简历，经过我们评估，您与该位职位符合度很高。我们会在稍后与用人部门继续
+协商，尽快确认是否给您发送面试邀请，请您关注后续状态。</h6>
+                                      <h6>----{{item.companyName}}</h6>
+                                  </dd>
+                              </dl>
+                          </a-timeline-item>
+                          <a-timeline-item v-if="item.read_time">
+                              <p>{{format(item.read_time)}}</p>
                               <dl>
                                   <dt><img :src="item.logo" alt=""></dt>
                                   <dd>
                                       <div>{{item.companyName}}</div>
-                                      <div v-if="item.is_read == '0'">已成功接收你的简历</div>
-                                      <div v-if="item.is_read == '1'">查看了你的简历</div>
+                                      <div>查看了你的简历</div>
+                                  </dd>
+                              </dl>
+                          </a-timeline-item>
+                          <a-timeline-item>
+                              <p>{{format(item.send_time)}}</p>
+                              <dl>
+                                  <dt><img :src="item.logo" alt=""></dt>
+                                  <dd>
+                                      <div>{{item.companyName}}</div>
+                                      <div>已成功接收你的简历</div>
                                   </dd>
                               </dl>
                           </a-timeline-item>
@@ -116,6 +152,16 @@ export default {
   },
   methods: {
       format: util.format,
+      modify(e, index) {
+          ajax.put('company/resumes', {
+              id: e.id,
+              status: index
+          }).then(res => {
+              if(res.retcode == 0) {
+                  this.devData();
+              }
+          })
+      },
       handleNav(index) {
           this.active = index;
           let _list = [];
@@ -148,7 +194,8 @@ export default {
                       item.steps = ['投递成功', '简历被查看', '有意向', '邀面试'];
                       item.isIndex = 2;
                       item.activeName = '有意向';
-                      if(item.is_interview == '2') {
+                      if(item.is_read != 2) item.isShow = true;
+                      if(item.is_interview == '2' && item.is_read != 2) {
                           _list.push(item);
                       }
                   });
@@ -162,6 +209,7 @@ export default {
                       item.steps = ['投递成功', '简历被查看', '有意向', '不合适'];
                       item.isIndex = 3;
                       item.activeName = '不合适';
+                      item.isShow = true;
                       if(item.is_read == '2') {
                           _list.push(item);
                       }
@@ -173,6 +221,7 @@ export default {
           }
       },
       handleInterviews(index) {
+          this.isTrue = false;
           let _list = [];
           switch (index) {
               case 0:
@@ -180,7 +229,8 @@ export default {
                       item.steps = ['投递成功', '简历被查看', '有意向', '邀面试'];
                       item.isIndex = 3;
                       item.activeName = '邀面试';
-                      if(item.is_interview == '1') {
+                      if(item.is_read != 2) item.isShow = true;
+                      if(item.is_interview == '1' && item.is_read != 2) {
                           _list.push(item);
                       }
                   });
@@ -190,8 +240,9 @@ export default {
                   this.rows.map(item => {
                       item.steps = ['投递成功', '简历被查看', '有意向', '邀面试'];
                       item.isIndex = 3;
-                      item.activeName = '邀面试';
-                      if(item.is_interview == '1' && item.status == '0') {
+                      item.activeName = '待确认';
+                      if(item.is_read != 2) item.isShow = true;
+                      if(item.is_interview == '1' && item.status == '0' && item.is_read != 2) {
                           _list.push(item);
                       }
                   });
@@ -202,7 +253,8 @@ export default {
                       item.steps = ['投递成功', '简历被查看', '有意向', '邀面试'];
                       item.isIndex = 3;
                       item.activeName = '已接受';
-                      if(item.is_interview == '1' && item.status == '1') {
+                      if(item.is_read != 2) item.isShow = true;
+                      if(item.is_interview == '1' && item.status == '1' && item.is_read != 2) {
                           _list.push(item);
                       }
                   });
@@ -213,7 +265,8 @@ export default {
                       item.steps = ['投递成功', '简历被查看', '有意向', '邀面试'];
                       item.isIndex = 3;
                       item.activeName = '已拒绝';
-                      if(item.is_interview == '1' && item.status == '2') {
+                      if(item.is_read != 2) item.isShow = true;
+                      if(item.is_interview == '1' && item.status == '2' && item.is_read != 2) {
                           _list.push(item);
                       }
                   });
@@ -223,7 +276,6 @@ export default {
               default:
                   break;
           };
-          this.isTrue = false;
       },
       refresh() {
           this.devData();
@@ -236,16 +288,7 @@ export default {
           }).then(res => {
               if(res.retcode == 0) {
                   this.rows = res.data || [];
-                  let _list = [];
-                  this.rows.map(item => {
-                      item.steps = ['投递成功', '简历被查看', '有意向', '邀面试'];
-                      item.isIndex = 0;
-                      item.activeName = '投递成功';
-                      if(item.is_read == '0') {
-                          _list.push(item);
-                      }
-                  });
-                  this.list = _list;
+                  this.handleNav(this.active);
               }
           })
       }

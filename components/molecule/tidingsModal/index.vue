9b75edb70c8a699fb7949e16b294d45677935659
dengxinliang <645068340@qq.com>
2019-09-25 17:59:47 +0800

    二期修改

---
+++ components/molecule/tidingsModal/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <section>
+      <div class="tidings">
+          <div class="table">
+              <p v-for="(item, index) in list" :key="index" :class="{read: item.isRead}">
+                  <span>收到{{item.name}}的一份简历投递</span>
+                  <span>{{item.times}}</span>
+              </p>
+          </div>
+          <a-pagination showQuickJumper :total="list.length" />
+      </div>
+  </section>
+</template>
+
+<script>
+import "./index.less";
+
+export default {
+  data() {
+      return{
+          list: [
+              {
+                  name: '某某',
+                  times: '2019.06.28 18:30',
+                  isRead: false
+              },
+              {
+                  name: '某某',
+                  times: '2019.06.28 18:30',
+                  isRead: false
+              },
+              {
+                  name: '某某某',
+                  times: '2019.06.28 18:30',
+                  isRead: true
+              }
+          ]
+      }
+  },
+  mounted() {
+  },
+  methods: {
+  }
+};
+</script>
